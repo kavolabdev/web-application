2677c891d2ef4dabfd53f96fea30b310dcac07e8
cukurova-ind <cukurovaindustrialprojects@gmail.com>
2025-06-02 17:27:04 +0300

    from simple to hard

--- gateway/static/main.js
+++ gateway/static/main.js
@@ -2,11 +2,6 @@ let socket;
 
 $(document).ready(function () {
   
-  $.get('/ports', function (ports) {
-    ports.forEach(p => {
-      $('#ports').append(`<option value="${p}">${p}</option>`);
-    });
-  });
 
   $('#sendBtn').click(async function () {
 
@@ -14,7 +9,7 @@ $(document).ready(function () {
 
         const message = $('#dataInput').val();
         console.log(message);
-        
+
         const hexArray = message.split(" ");
         console.log(hexArray.length);
         const bufferToSend = [];
@@ -26,17 +21,6 @@ $(document).ready(function () {
 
         const port = await navigator.serial.requestPort();
 
-        const info = selectedPort.getInfo();
-        let displayName = "Port selected.";
-        if (info.usbVendorId && info.usbProductId) {
-        displayName += ` USB Vendor: 0x${info.usbVendorId.toString(16)}, Product: 0x${info.usbProductId.toString(16)}`;
-        } else {
-        displayName += " (No USB info)";
-        }
-
-        $('#selectedPortInfo').text(displayName);
-
-
         const opts = {
           baudRate: 57600,
           dataBits: 8,
@@ -46,40 +30,6 @@ $(document).ready(function () {
         }
         await port.open(opts);
         console.log("port opened");
-        
-        const writer = port.writable.getWriter();
-        await writer.write(new Uint8Array(bufferToSend));
-        writer.releaseLock();
-
-        console.log("Data sent. Waiting to read...");
-        const reader = port.readable.getReader();
-        const output = $('#receivedOutput');
-        let fullResponse = [];
-
-        console.log("Reading data...");
-        let totalBytesRead = 0;
-        const maxBytes = 60;
-        while (totalBytesRead < maxBytes) {
-          const { value, done } = await reader.read();
-          if (done) {
-            break;
-          }
-          if (value && value.length > 0) {
-            const remaining = maxBytes - totalBytesRead;
-            const slice = value.slice(0, remaining);
-            fullResponse.push(...slice);
-            totalBytesRead += slice.length;
-          }
-        }
-
-        reader.releaseLock();
-        await port.close();
-
-        const hexString = fullResponse.map(b => b.toString(16).padStart(2, '0')).join(' ');
-        console.log("Total Bytes Received:", totalBytesRead);
-        console.log("Hex String:", hexString);
-        output.text(hexString);
-
 
       } catch (err) {
         console.error("serial port error:", err);
