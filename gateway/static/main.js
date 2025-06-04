3413a7f01edcbedf345df3d47bc2e67c23189de9
cukurova-ind <cukurovaindustrialprojects@gmail.com>
2025-06-04 10:26:01 +0300

    serial port basic messaging

--- gateway/static/main.js
+++ gateway/static/main.js
@@ -31,6 +31,39 @@ $(document).ready(function () {
         await port.open(opts);
         console.log("port opened");
 
+        const writer = port.writable.getWriter();
+        await writer.write(new Uint8Array(bufferToSend));
+        writer.releaseLock();
+
+        console.log("Data sent. Waiting to read...");
+        const reader = port.readable.getReader();
+        const output = $('#receivedOutput');
+        let fullResponse = [];
+
+        console.log("Reading data...");
+        let totalBytesRead = 0;
+        const maxBytes = 60;
+        while (totalBytesRead < maxBytes) {
+          const { value, done } = await reader.read();
+          if (done) {
+            break;
+          }
+          if (value && value.length > 0) {
+            const remaining = maxBytes - totalBytesRead;
+            const slice = value.slice(0, remaining);
+            fullResponse.push(...slice);
+            totalBytesRead += slice.length;
+          }
+        }
+
+        reader.releaseLock();
+        await port.close();
+
+        const hexString = fullResponse.map(b => b.toString(16).padStart(2, '0')).join(' ');
+        console.log("Total Bytes Received:", totalBytesRead);
+        console.log("Hex String:", hexString);
+        output.text(hexString);
+
       } catch (err) {
         console.error("serial port error:", err);
       }
